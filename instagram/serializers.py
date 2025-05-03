9bfb4d264a8ac29da4acf2e459b440e53568ecba
Fouziya-12 <fouziyapathan12@gmail.com>
2025-05-03 12:27:24 +0530

    Get user post

--- instagram/serializers.py
+++ instagram/serializers.py
@@ -36,3 +36,13 @@ class PostSerializer(serializers.ModelSerializer):
         model = Post
         fields = ['id','user','post_url','content','created_at']
         read_only_fields = ['user','created_at']
+
+class UserGetPostSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(source='user.username',read_only=True)
+    email = serializers.EmailField(source='user.email',read_only=True)
+    
+    class Meta:
+        model = Post
+        fields = ['username','email','user','post_url','content','created_at']
+
+        
--- instagram/views.py
+++ instagram/views.py
@@ -1,7 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from .serializers import RegisterSerializer,LoginSerializer,PostSerializer
+from .serializers import RegisterSerializer,LoginSerializer,PostSerializer,UserGetPostSerializer
 from rest_framework_simplejwt.tokens import RefreshToken
 from .models import CustomUser,Post
 from rest_framework.permissions import IsAuthenticated
@@ -62,3 +62,11 @@ class CreatePostView(APIView):
             serializer.save(user=request.user)  #set logged-in user
             return Response(serializer.data,status=status.HTTP_201_CREATED)
         return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+    
+class UserGetPostView(APIView):
+    permission_classes = [IsAuthenticated]
+    
+    def get(self,request):
+        user_posts = Post.objects.filter(user=request.user)
+        serializer = UserGetPostSerializer(user_posts,many=True)
+        return Response(serializer.data)
